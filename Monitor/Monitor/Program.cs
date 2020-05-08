0d0d64ac13fa8314e59c06f8f799ae342c0dda48
SogAsh <sogdian4815@mail.ru>
2020-05-08 15:51:31 +0500

    Initial commit

---
+++ Monitor/Monitor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Monitor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameProcess = args[0].ToString();
+            var keepAlive = int.Parse(args[1].ToString());
+            var frequencyMonitor = int.Parse(args[2].ToString());
+
+            while (true)
+            {
+                var targetProcesses = Process.GetProcessesByName(nameProcess);
+                if (targetProcesses.Count() > 0)
+                    foreach (var proc in targetProcesses)
+                    {
+                        var currentTime = DateTime.Now.Subtract(proc.StartTime).TotalMilliseconds;
+                        if (currentTime > keepAlive * 60 * 1000)
+                            proc.Kill();
+                    }
+                Thread.Sleep(frequencyMonitor * 60 * 1000);
+            }
+        }
+      
+    }
+}
